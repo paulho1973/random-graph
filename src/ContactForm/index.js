5fc2ad27f68a84b8cf4d79831e699151f56d3af8
YONG HING HO <paulho1973@outlook.com>
2022-01-12 18:10:58 +0800

    commit 9

--- src/ContactForm/index.js
+++ src/ContactForm/index.js
@@ -1,10 +1,49 @@
 import React from 'react';
 
+import styles from './ContactForm.module.css';
+
 export default function ContactForm() {
 
   return (
     <div>
-
+      <div className={styles.contain}>
+        <div className={styles.contacts}>
+          <h3 className={styles.h3}>Our contacts</h3>
+          <ul className={styles.ul}>
+            <li>San Joe St.</li>
+            <li>00-1212121-11</li>
+            <li>mail@mail.com</li>
+          </ul>
+        </div>
+        <div className={styles.formfill}>
+          <h3 className={styles.h3}>Send us a message</h3>        
+          <form className={styles.form} action>       
+            <p className={styles.p}>
+              <label className={styles.label} htmlFor>Your name</label>
+              <input className={styles.input} type="text" />
+            </p>
+            <p className={styles.p}>
+              <label className={styles.label} htmlFor>Skype</label>
+              <input className={styles.input} type="text" />
+            </p>
+            <p className={styles.p}>
+              <label className={styles.label} htmlFor>Email Address</label>
+              <input className={styles.input} type="text" />
+            </p>
+            <p className={styles.p}>
+              <label className={styles.label} htmlFor>Topic</label>
+              <input className={styles.input} type="text" />
+            </p>
+            <p className={styles.fullwidth}>
+              <label className={styles.label} htmlFor>Write your message</label>
+              <textarea className={styles.textarea} cols={30} rows={7} defaultValue={""} />
+            </p>
+            <p className={styles.fullwidth}>
+              <button className={styles.button}>Send</button>
+            </p>
+          </form> 
+        </div>
+      </div>
     </div>
   );
 }
--- src/ProfileForm/index.js
+++ src/ProfileForm/index.js
@@ -6,19 +6,7 @@ export default function ProfileForm() {
 
   return (
     <div className={styles}>
-      <form>
-        <label htmlFor="firstName" >First Name</label>
-        <input id="firstName" type="text" />
-        <label htmlFor="lastName" >Last Name</label>
-        <input id="lastName" type="text" />
-        <label htmlFor="job">Job</label>
-        <input id="job" type="text" />
-        <label htmlFor="age">Age</label>
-        <input id="age" type="text" />
-        <label htmlFor="email">Email</label>
-        <input id="email" type="email" />
-        <button>Submit</button>
-      </form>
+
     </div>
   );
 }
