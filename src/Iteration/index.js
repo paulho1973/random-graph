fae41781cef1cf9fede9804105ee24c0c12f3728
YONG HING HO <paulho1973@outlook.com>
2022-01-31 07:28:57 +0800

    commit 21

--- src/Iteration/index.js
+++ src/Iteration/index.js
@@ -6,10 +6,32 @@ const Iteration = () => {
     for (var i = 0; i < 30; i++) {
         rows.push(<h2 key={i}>Row {i}</h2>);
     }
-    
+    var data = ["Apple",
+				"Orange",
+				"Banana",
+				"Pineapple",
+				"Honeydew"
+			   ]
+
+	var datarows = data.map((item, i) => 
+						<h2 key={i}>
+							{item}
+						</h2>
+					)
+
 	return (
 		<div>
 			{rows}	
+
+			{
+				data.map((item, i) => 
+					<h2 key={i}>
+						{item}
+					</h2>
+				)
+            }
+
+			{datarows}
 		</div>
 	);
 }
